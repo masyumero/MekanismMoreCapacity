049068293f33bda83b9fdbddba8bf0ad98a81a81
masyumero <167562220+masyumero@users.noreply.github.com>
2025-04-18 14:52:40 +0900

    config test

--- src/main/java/io/github/masyumero/mekanismmorecapacity/MekanismMoreCapacity.java
+++ src/main/java/io/github/masyumero/mekanismmorecapacity/MekanismMoreCapacity.java
@@ -1,6 +1,10 @@
 package io.github.masyumero.mekanismmorecapacity;
 
+import io.github.masyumero.mekanismmorecapacity.common.config.MMCConfig;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+
+import java.nio.file.Path;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MekanismMoreCapacity.MODID)
@@ -8,4 +12,9 @@ public class MekanismMoreCapacity {
 
     // Define mod id in a common place for everything to reference
     public static final String MODID = "mekanismmorecapacity";
+    public static final String MODNAME = "MekanismMoreCapacity";
+
+    public MekanismMoreCapacity() {
+        MMCConfig.registerConfigs(ModLoadingContext.get());
+    }
 }
