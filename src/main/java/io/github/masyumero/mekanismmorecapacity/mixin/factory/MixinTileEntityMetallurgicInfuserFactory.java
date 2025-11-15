ef1ff903a0031c50e7a536b45bc2beb45d9e2996
masyumero <167562220+masyumero@users.noreply.github.com>
2025-11-15 17:08:34 +0900

    Bump mod version to 1.4.0

--- src/main/java/io/github/masyumero/mekanismmorecapacity/mixin/factory/MixinTileEntityMetallurgicInfuserFactory.java
+++ src/main/java/io/github/masyumero/mekanismmorecapacity/mixin/factory/MixinTileEntityMetallurgicInfuserFactory.java
@@ -2,7 +2,6 @@ package io.github.masyumero.mekanismmorecapacity.mixin.factory;
 
 import io.github.masyumero.mekanismmorecapacity.common.config.MMCConfig;
 import io.github.masyumero.mekanismmorecapacity.common.util.TierUtil;
-import mekanism.api.chemical.infuse.IInfusionTank;
 import mekanism.api.chemical.infuse.InfuseType;
 import mekanism.api.chemical.infuse.InfusionStack;
 import mekanism.api.providers.IBlockProvider;
@@ -16,7 +15,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
 
@@ -25,9 +23,6 @@ import java.util.Set;
 
 @Mixin(value = TileEntityMetallurgicInfuserFactory.class,remap = false)
 public abstract class MixinTileEntityMetallurgicInfuserFactory extends TileEntityItemToItemFactory<MetallurgicInfuserRecipe> implements IHasDumpButton, IDoubleRecipeLookupHandler.ItemChemicalRecipeLookupHandler<InfuseType, InfusionStack, MetallurgicInfuserRecipe> {
-
-    @Shadow
-    public IInfusionTank infusionTank;
 
     protected MixinTileEntityMetallurgicInfuserFactory(IBlockProvider blockProvider, BlockPos pos, BlockState state, List<CachedRecipe.OperationTracker.RecipeError> errorTypes, Set<CachedRecipe.OperationTracker.RecipeError> globalErrorTypes) {
         super(blockProvider, pos, state, errorTypes, globalErrorTypes);
