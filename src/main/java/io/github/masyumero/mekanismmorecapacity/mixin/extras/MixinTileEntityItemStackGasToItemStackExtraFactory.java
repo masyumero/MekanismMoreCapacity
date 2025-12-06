ab2f864c8a14365121043d2309df06f191cb6d4a
masyumero <167562220+masyumero@users.noreply.github.com>
2025-12-06 14:22:27 +0900

    Refactor gas and infusion tank initialization to use modified capacity values

--- src/main/java/io/github/masyumero/mekanismmorecapacity/mixin/extras/MixinTileEntityItemStackGasToItemStackExtraFactory.java
+++ src/main/java/io/github/masyumero/mekanismmorecapacity/mixin/extras/MixinTileEntityItemStackGasToItemStackExtraFactory.java
@@ -3,29 +3,20 @@ package io.github.masyumero.mekanismmorecapacity.mixin.extras;
 import com.jerry.mekanism_extras.common.tile.factory.TileEntityItemStackGasToItemStackExtraFactory;
 import com.jerry.mekanism_extras.common.tile.factory.TileEntityItemToItemExtraFactory;
 import io.github.masyumero.mekanismmorecapacity.common.config.MMCConfig;
-import mekanism.api.IContentsListener;
-import mekanism.api.chemical.ChemicalTankBuilder;
 import mekanism.api.chemical.gas.Gas;
 import mekanism.api.chemical.gas.GasStack;
-import mekanism.api.chemical.gas.IGasTank;
 import mekanism.api.providers.IBlockProvider;
 import mekanism.api.recipes.ItemStackGasToItemStackRecipe;
 import mekanism.api.recipes.cache.CachedRecipe;
-import mekanism.common.block.attribute.Attribute;
-import mekanism.common.block.attribute.AttributeFactoryType;
-import mekanism.common.capabilities.holder.chemical.ChemicalTankHelper;
-import mekanism.common.capabilities.holder.chemical.IChemicalTankHolder;
-import mekanism.common.content.blocktype.FactoryType;
 import mekanism.common.recipe.lookup.IDoubleRecipeLookupHandler;
 import mekanism.common.recipe.lookup.IRecipeLookupHandler;
 import mekanism.common.tile.interfaces.IHasDumpButton;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.List;
 import java.util.Set;
@@ -38,21 +29,14 @@ public abstract class MixinTileEntityItemStackGasToItemStackExtraFactory extends
         super(blockProvider, pos, state, errorTypes, globalErrorTypes);
     }
 
-    @Shadow
-    public IGasTank gasTank;
+    @ModifyArg(method = "getInitialGasTanks",at = @At(value = "INVOKE", target = "Lmekanism/api/chemical/ChemicalTankBuilder;create(JLjava/util/function/Predicate;Lmekanism/api/IContentsListener;)Lmekanism/api/chemical/IChemicalTank;"))
+    public long createModifyArg(long capacity) {
+        return mekanismMoreCapacity$getConfigValue();
+    }
 
-    @Redirect(method = "getInitialGasTanks",at = @At(value = "INVOKE", target = "Lmekanism/common/capabilities/holder/chemical/ChemicalTankHelper;build()Lmekanism/common/capabilities/holder/chemical/IChemicalTankHolder;"))
-    public IChemicalTankHolder<Gas, GasStack, IGasTank> getInitialGasTanks(ChemicalTankHelper instance, IContentsListener listener) {
-        ChemicalTankHelper<Gas, GasStack, IGasTank> builder = ChemicalTankHelper.forSideGasWithConfig(this::getDirection, this::getConfig);
-        if (allowExtractingChemical()) {
-            gasTank = ChemicalTankBuilder.GAS.create(mekanismMoreCapacity$getConfigValue(), this::containsRecipeB,
-                    markAllMonitorsChanged(listener));
-        } else {
-            gasTank = ChemicalTankBuilder.GAS.input(mekanismMoreCapacity$getConfigValue(), this::containsRecipeB,
-                    markAllMonitorsChanged(listener));
-        }
-        builder.addTank(gasTank);
-        return builder.build();
+    @ModifyArg(method = "getInitialGasTanks",at = @At(value = "INVOKE", target = "Lmekanism/api/chemical/ChemicalTankBuilder;input(JLjava/util/function/Predicate;Lmekanism/api/IContentsListener;)Lmekanism/api/chemical/IChemicalTank;"))
+    public long inputModifyArg(long capacity) {
+        return mekanismMoreCapacity$getConfigValue();
     }
 
     @Unique
@@ -63,10 +47,5 @@ public abstract class MixinTileEntityItemStackGasToItemStackExtraFactory extends
             case COSMIC -> MMCConfig.MEK_EXTRAS_MACHINE_CONFIG.CosmicFactories.get();
             case INFINITE -> MMCConfig.MEK_EXTRAS_MACHINE_CONFIG.InfiniteFactories.get();
         };
-    }
-
-    @Shadow
-    private boolean allowExtractingChemical() {
-        return Attribute.get(blockProvider, AttributeFactoryType.class).getFactoryType() == FactoryType.COMPRESSING;
     }
 }
--- src/main/java/io/github/masyumero/mekanismmorecapacity/mixin/extras/MixinTileEntityMetallurgicInfuserExtraFactory.java
+++ src/main/java/io/github/masyumero/mekanismmorecapacity/mixin/extras/MixinTileEntityMetallurgicInfuserExtraFactory.java
@@ -3,25 +3,19 @@ package io.github.masyumero.mekanismmorecapacity.mixin.extras;
 import com.jerry.mekanism_extras.common.tile.factory.TileEntityItemToItemExtraFactory;
 import com.jerry.mekanism_extras.common.tile.factory.TileEntityMetallurgicInfuserExtraFactory;
 import io.github.masyumero.mekanismmorecapacity.common.config.MMCConfig;
-import mekanism.api.IContentsListener;
-import mekanism.api.chemical.ChemicalTankBuilder;
-import mekanism.api.chemical.infuse.IInfusionTank;
 import mekanism.api.chemical.infuse.InfuseType;
 import mekanism.api.chemical.infuse.InfusionStack;
 import mekanism.api.providers.IBlockProvider;
 import mekanism.api.recipes.MetallurgicInfuserRecipe;
 import mekanism.api.recipes.cache.CachedRecipe;
-import mekanism.common.capabilities.holder.chemical.ChemicalTankHelper;
-import mekanism.common.capabilities.holder.chemical.IChemicalTankHolder;
 import mekanism.common.recipe.lookup.IDoubleRecipeLookupHandler;
 import mekanism.common.tile.interfaces.IHasDumpButton;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.List;
 import java.util.Set;
@@ -30,25 +24,13 @@ import java.util.Set;
 public abstract class MixinTileEntityMetallurgicInfuserExtraFactory extends TileEntityItemToItemExtraFactory<MetallurgicInfuserRecipe> implements IHasDumpButton,
         IDoubleRecipeLookupHandler.ItemChemicalRecipeLookupHandler<InfuseType, InfusionStack, MetallurgicInfuserRecipe> {
 
-    @Shadow
-    IInfusionTank infusionTank;
-
     protected MixinTileEntityMetallurgicInfuserExtraFactory(IBlockProvider blockProvider, BlockPos pos, BlockState state, List<CachedRecipe.OperationTracker.RecipeError> errorTypes, Set<CachedRecipe.OperationTracker.RecipeError> globalErrorTypes) {
         super(blockProvider, pos, state, errorTypes, globalErrorTypes);
     }
 
-    @Redirect(method = "getInitialInfusionTanks",at = @At(value = "INVOKE", target = "Lmekanism/common/capabilities/holder/chemical/ChemicalTankHelper;build()Lmekanism/common/capabilities/holder/chemical/IChemicalTankHolder;"))
-    public IChemicalTankHolder<InfuseType, InfusionStack, IInfusionTank> getInitialInfusionTanks(ChemicalTankHelper instance, IContentsListener listener) {
-        ChemicalTankHelper<InfuseType, InfusionStack, IInfusionTank> builder = ChemicalTankHelper.forSideInfusionWithConfig(this::getDirection, this::getConfig);
-        builder.addTank(infusionTank = ChemicalTankBuilder.INFUSION.create(mekanismMoreCapacity$getConfigValue(), this::containsRecipeB,
-                markAllMonitorsChanged(listener)));
-        return builder.build();
-    }
-
-
-    @Unique
-    private String mekanismMoreCapacity$getTier() {
-        return tier.getAdvanceTier().getSimpleName();
+    @ModifyArg(method = "getInitialInfusionTanks",at = @At(value = "INVOKE", target = "Lmekanism/api/chemical/ChemicalTankBuilder;create(JLjava/util/function/Predicate;Lmekanism/api/IContentsListener;)Lmekanism/api/chemical/IChemicalTank;"))
+    public long createModifyArg(long capacity) {
+        return mekanismMoreCapacity$getConfigValue();
     }
 
     @Unique
