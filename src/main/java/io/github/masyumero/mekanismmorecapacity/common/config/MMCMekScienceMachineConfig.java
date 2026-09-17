05ccfe4f8dad829f0cb4a62ad9ef6d8e027586b2
masyumero <167562220+masyumero@users.noreply.github.com>
2026-09-18 04:09:21 +0900

    feat(mixin): add Infinity Ore Reprocessing mixin and config * Introduced MixinTileEntityInfinityOreReprocessing to modify input capacity. * Updated MMCMekScienceMachineConfig to include infinityOreReprocessing capacity. * Reorganized mixin structure for better clarity and maintainability.

--- src/main/java/io/github/masyumero/mekanismmorecapacity/common/config/MMCMekScienceMachineConfig.java
+++ src/main/java/io/github/masyumero/mekanismmorecapacity/common/config/MMCMekScienceMachineConfig.java
@@ -16,6 +16,7 @@ public class MMCMekScienceMachineConfig extends BaseMekanismConfig {
     public final CachedLongValue AirCompressor;
     public final CachedIntValue AdsorptionSeparatorInput;
     public final CachedLongValue AdsorptionSeparatorOutput;
+    public final CachedLongValue infinityOreReprocessing;
 
     public MMCMekScienceMachineConfig() {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
@@ -32,6 +33,8 @@ public class MMCMekScienceMachineConfig extends BaseMekanismConfig {
         builder.pop().push("AdsorptionSeparator");
         AdsorptionSeparatorInput = CachedIntValue.wrap(this, builder.comment("Chemical tank capacity (mB). Default: 1000000, Vanilla: 10000").defineInRange("inputFluidTankCapacity", 1000000, 1, Integer.MAX_VALUE));
         AdsorptionSeparatorOutput = CachedLongValue.wrap(this, builder.comment("Chemical tank capacity (mB). Default: 1000000, Vanilla: 10000").defineInRange("outputChemicalTankCapacity", 1000000, 1, Long.MAX_VALUE));
+        builder.pop().push("InfinityOreReprocessing");
+        infinityOreReprocessing = CachedLongValue.wrap(this, builder.comment("Chemical tank capacity (mB). Default: 1000000, Vanilla: 10000").defineInRange("outputChemicalTankCapacity", 1000000, 1, Long.MAX_VALUE));
         configSpec = builder.build();
     }
 
